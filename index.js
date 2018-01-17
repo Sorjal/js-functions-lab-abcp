b6b012e701ebe65e88c931a7ca99cd05ab948203
Joseph Birish <joe.birish@gmail.com>
2018-01-17 16:46:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+var name = 'you';
+
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo('you') {
+  return "Happy holidays, ${name}!"
+}
